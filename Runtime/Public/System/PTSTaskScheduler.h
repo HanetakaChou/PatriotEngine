d8df25e1d17d85af6ddfd503508d6a2b4fa30479
请叫我红领巾 <553812616@qq.com>
2018-05-02 16:53:11 +0800

    no commit message

--- Runtime/Public/System/PTSTaskScheduler.h
+++ Runtime/Public/System/PTSTaskScheduler.h
@@ -38,13 +38,13 @@ public:
 	inline void RefCount_Set(uint32_t RefCount);
 
 	template<typename TaskImpl>
-	static inline void * Allocate_Root(TaskImpl *, IPTSTaskScheduler *pTaskScheduler = NULL);
+	static inline void * Allocate_Root(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler = NULL);
 
 	template<typename TaskImpl>
-	inline void * Allocate_Child(TaskImpl *, IPTSTaskScheduler *pTaskScheduler = NULL);
+	inline void * Allocate_Child(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler = NULL);
 
 	template<typename TaskImpl>
-	inline void * Allocate_Continuation(TaskImpl *, IPTSTaskScheduler *pTaskScheduler = NULL);
+	inline void * Allocate_Continuation(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler = NULL);
 };
 
 struct IPTSTaskScheduler
@@ -86,8 +86,10 @@ inline void IPTSTask::RefCount_Set(uint32_t RefCount)
 }
 
 template<typename TaskImpl>
-inline void * IPTSTask::Allocate_Root(TaskImpl *, IPTSTaskScheduler *pTaskScheduler)
+inline void * IPTSTask::Allocate_Root(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler)
 {
+	assert(pTaskNull == NULL);
+
 	if (pTaskScheduler == NULL)
 	{
 		pTaskScheduler = ::PTSTaskScheduler_ForThread();
@@ -99,8 +101,10 @@ inline void * IPTSTask::Allocate_Root(TaskImpl *, IPTSTaskScheduler *pTaskSchedu
 }
 
 template<typename TaskImpl>
-inline void * IPTSTask::Allocate_Child(TaskImpl *, IPTSTaskScheduler *pTaskScheduler)
+inline void * IPTSTask::Allocate_Child(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler)
 {
+	assert(pTaskNull == NULL);
+
 	if (pTaskScheduler == NULL)
 	{
 		pTaskScheduler = ::PTSTaskScheduler_ForThread();
@@ -116,8 +120,10 @@ inline void * IPTSTask::Allocate_Child(TaskImpl *, IPTSTaskScheduler *pTaskSched
 }
 
 template<typename TaskImpl>
-inline void * IPTSTask::Allocate_Continuation(TaskImpl *, IPTSTaskScheduler *pTaskScheduler)
+inline void * IPTSTask::Allocate_Continuation(TaskImpl *pTaskNull, IPTSTaskScheduler *pTaskScheduler)
 {
+	assert(pTaskNull == NULL);
+
 	if (pTaskScheduler == NULL)
 	{
 		pTaskScheduler = ::PTSTaskScheduler_ForThread();
@@ -154,26 +160,26 @@ namespace __PTSInternal_Parallel_Map
 		}
 	};
 
-	template<typename PTSLambda_Serial_Map>
+	template<typename PTSTYPE_Lambda_Serial_Map>
 	class PTSTask_Map :public IPTSTask
 	{
 		uint32_t const m_Threshold;
 		uint32_t m_Begin;
 		uint32_t m_End;
-		PTSLambda_Serial_Map const &m_rLambdaSerialMap;
+		PTSTYPE_Lambda_Serial_Map const &m_rSerialMap;
 
 	public:
 		inline PTSTask_Map(
 			uint32_t Threshold,
 			uint32_t Begin,
 			uint32_t End,
-			PTSLambda_Serial_Map const &rLambdaSerialMap
+			PTSTYPE_Lambda_Serial_Map const &rSerialMap
 		)
 			:
 			m_Threshold(Threshold),
 			m_Begin(Begin),
 			m_End(End),
-			m_rLambdaSerialMap(rLambdaSerialMap)
+			m_rSerialMap(rSerialMap)
 		{
 
 		}
@@ -182,10 +188,10 @@ namespace __PTSInternal_Parallel_Map
 		{
 			IPTSTaskScheduler *pTaskScheduler = ::PTSTaskScheduler_ForThread();//Master And Worker Divergence
 
-			if ((m_End - m_Begin) > m_Threshold) //Recursive
+			if ((this->m_End - this->m_Begin) > this->m_Threshold) //Recursive
 			{
 				//Divide
-				uint32_t Middle = m_Begin + (m_End - m_Begin) / 2U;
+				uint32_t Middle = this->m_Begin + (this->m_End - this->m_Begin) / 2U;
 
 				//Continuation Passing Style
 				PTSTask_Map_Continuation *pTaskContinuation = NULL;
@@ -193,10 +199,10 @@ namespace __PTSInternal_Parallel_Map
 					this->Allocate_Continuation(pTaskContinuation, pTaskScheduler)
 					) PTSTask_Map_Continuation{};
 
-				PTSTask_Map *pTaskChildRight = NULL;
+				PTSTask_Map<PTSTYPE_Lambda_Serial_Map> *pTaskChildRight = NULL;
 				pTaskChildRight = new(
 					pTaskContinuation->Allocate_Child(pTaskChildRight, pTaskScheduler)
-					)PTSTask_Map(m_Threshold, Middle, m_End, m_rLambdaSerialMap);
+					)PTSTask_Map<PTSTYPE_Lambda_Serial_Map>(this->m_Threshold, Middle, this->m_End, this->m_rSerialMap);
 
 				//Scheduler ByPass
 				this->Recycle_AsChildOf(pTaskContinuation);
@@ -210,29 +216,164 @@ namespace __PTSInternal_Parallel_Map
 			}
 			else //Base Case
 			{
-				m_rLambdaSerialMap(m_Begin, m_End);
+				m_rSerialMap(this->m_Begin, this->m_End);
 				return NULL;
 			}
 		}
 	};
 }
 
-template<typename PTSLambda_Serial_Map>
+template<typename PTSTYPE_Lambda_Serial_Map>
 inline void PTSParallel_Map(
 	uint32_t Threshold,
 	uint32_t Begin,
 	uint32_t End,
-	PTSLambda_Serial_Map const &rLambdaSerialMap
+	PTSTYPE_Lambda_Serial_Map const &rSerialMap
 )
 {
 	IPTSTaskScheduler *pTaskScheduler = ::PTSTaskScheduler_ForThread();
 
-	__PTSInternal_Parallel_Map::PTSTask_Map<PTSLambda_Serial_Map> *pTaskMap = NULL;
+	__PTSInternal_Parallel_Map::PTSTask_Map<PTSTYPE_Lambda_Serial_Map> *pTaskMap = NULL;
 	pTaskMap = new(
 		IPTSTask::Allocate_Root(pTaskMap, pTaskScheduler)
-		)__PTSInternal_Parallel_Map::PTSTask_Map<PTSLambda_Serial_Map>(Threshold, Begin, End, rLambdaSerialMap);
+		)__PTSInternal_Parallel_Map::PTSTask_Map<PTSTYPE_Lambda_Serial_Map>(Threshold, Begin, End, rSerialMap);
 
 	pTaskScheduler->Task_WaitRoot(pTaskMap);
+}
+
+namespace __PTSInternal_Parallel_Reduce
+{
+	template<typename PTSTYPE_Value, typename PTSTYPE_Lambda_Function_Combine>
+	class PTSTask_Reduce_Continuation :public IPTSTask
+	{
+		PTSTYPE_Value *const m_pResultRoot;
+		PTSTYPE_Lambda_Function_Combine const &m_rFunctionCombine;
+	public:
+		PTSTYPE_Value m_ResultChildLeft;
+		PTSTYPE_Value m_ResultChildRight;
+
+		inline PTSTask_Reduce_Continuation(
+			PTSTYPE_Value *pResultRoot,
+			PTSTYPE_Lambda_Function_Combine const &rFunctionCombine)
+			:
+			m_pResultRoot(pResultRoot),
+			m_rFunctionCombine(rFunctionCombine)
+		{
+
+		}
+
+		IPTSTask *Execute() override
+		{
+			//Conquer
+			(*this->m_pResultRoot) = m_rFunctionCombine(m_ResultChildLeft, m_ResultChildRight);
+			return NULL;
+		}
+	};
+
+	template<typename PTSTYPE_Value, typename PTSTYPE_Lambda_Serial_Reduce, typename PTSTYPE_Lambda_Function_Combine>
+	class PTSTask_Reduce :public IPTSTask
+	{
+		uint32_t const m_Threshold;
+		uint32_t m_Begin;
+		uint32_t m_End;
+		PTSTYPE_Value *m_pResultRoot;
+		PTSTYPE_Lambda_Serial_Reduce const &m_rSerialReduce;
+		PTSTYPE_Lambda_Function_Combine  const &m_rFunctionCombine;
+
+	public:
+		inline PTSTask_Reduce(
+			uint32_t Threshold,
+			uint32_t Begin,
+			uint32_t End,
+			PTSTYPE_Value *pResultRoot,
+			PTSTYPE_Lambda_Serial_Reduce const &rSerialReduce,
+			PTSTYPE_Lambda_Function_Combine const &rFunctionCombine
+		)
+			:
+			m_Threshold(Threshold),
+			m_Begin(Begin),
+			m_End(End),
+			m_pResultRoot(pResultRoot),
+			m_rSerialReduce(rSerialReduce),
+			m_rFunctionCombine(rFunctionCombine)
+		{
+
+		}
+
+		IPTSTask *Execute() override
+		{
+			IPTSTaskScheduler *pTaskScheduler = ::PTSTaskScheduler_ForThread();
+
+			if ((m_End - m_Begin) > m_Threshold) //Recursive
+			{
+				//Divide
+				uint32_t Middle = m_Begin + (m_End - m_Begin) / 2U;
+
+				//Continuation Passing Style
+				PTSTask_Reduce_Continuation<PTSTYPE_Value, PTSTYPE_Lambda_Function_Combine> *pTaskContinuation = NULL;
+				pTaskContinuation = new(
+					this->Allocate_Continuation(pTaskContinuation, pTaskScheduler)
+					) PTSTask_Reduce_Continuation<PTSTYPE_Value, PTSTYPE_Lambda_Function_Combine>(
+						this->m_pResultRoot,
+						this->m_rFunctionCombine
+						);
+
+				PTSTask_Reduce<PTSTYPE_Value, PTSTYPE_Lambda_Serial_Reduce, PTSTYPE_Lambda_Function_Combine> *pTaskChildRight = NULL;
+				pTaskChildRight = new(
+					pTaskContinuation->Allocate_Child(pTaskChildRight, pTaskScheduler)
+					)PTSTask_Reduce<PTSTYPE_Value, PTSTYPE_Lambda_Serial_Reduce, PTSTYPE_Lambda_Function_Combine>(
+						this->m_Threshold,
+						Middle, 
+						this->m_End,
+						&pTaskContinuation->m_ResultChildRight,
+						this->m_rSerialReduce,
+						this->m_rFunctionCombine
+					);
+
+				//Scheduler ByPass
+				this->Recycle_AsChildOf(pTaskContinuation);
+				this->m_End = Middle;
+				this->m_pResultRoot = &pTaskContinuation->m_ResultChildLeft;
+
+				pTaskContinuation->RefCount_Set(2);
+
+				pTaskScheduler->Task_Spawn(pTaskChildRight);
+
+				return this;
+			}
+			else //Base Case
+			{
+				(*this->m_pResultRoot) = m_rSerialReduce(this->m_Begin, this->m_End);
+				return NULL;
+			}
+		}
+	};
+}
+
+template<typename PTSTYPE_Value, typename PTSTYPE_Lambda_Serial_Reduce, typename PTSTYPE_Lambda_Function_Combine>
+inline void PTSParallel_Reduce(
+	uint32_t Threshold,
+	uint32_t Begin,
+	uint32_t End,
+	PTSTYPE_Value &rResult, //Initialize -> Identity
+	PTSTYPE_Lambda_Serial_Reduce const &rSerialReduce,
+	PTSTYPE_Lambda_Function_Combine const &rFunctionCombine
+)
+{
+	IPTSTaskScheduler *pTaskScheduler = ::PTSTaskScheduler_ForThread();
+
+	__PTSInternal_Parallel_Reduce::PTSTask_Reduce<PTSTYPE_Value, PTSTYPE_Lambda_Serial_Reduce, PTSTYPE_Lambda_Function_Combine> *pTaskReduce = NULL;
+	pTaskReduce = new(
+		IPTSTask::Allocate_Root(pTaskReduce, pTaskScheduler)
+		)__PTSInternal_Parallel_Reduce::PTSTask_Reduce<PTSTYPE_Value, PTSTYPE_Lambda_Serial_Reduce, PTSTYPE_Lambda_Function_Combine>(
+			Threshold, 
+			Begin, 
+			End, 
+			&rResult,
+			rSerialReduce,
+			rFunctionCombine);
+
+	pTaskScheduler->Task_WaitRoot(pTaskReduce);
 }
 
 #endif
