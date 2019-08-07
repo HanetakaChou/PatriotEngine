b0d53ce95af16d879dc88aee1ba6ca4400e9afb2
YuqiaoZhang <yuqiaozhang@aliyun.com>
2019-08-07 17:25:26 +0800

    Mach-O Support Universal Binary, We Must Depend On The Compiler Predefined Macros To Infer The Architecture.

--- Runtime/Public/PTCommon.h
+++ Runtime/Public/PTCommon.h
@@ -1,35 +1,90 @@
 ﻿#ifndef PT_COMMON_H
 #define PT_COMMON_H
 
-#if defined(PTWIN32)
-    #define PTCALL __stdcall
-    #define PTPTR __stdcall
-    #define PTIMPORT __declspec(dllimport)
-    #define PTEXPORT __declspec(dllexport)
-#elif defined(PTPOSIX)
-    #define PTCALL
-    #define PTPTR
-    #define PTIMPORT
-    #define PTEXPORT __attribute__((visibility("default")))
-    #if defined(PTPOSIXLINUX)
+#if defined(_MSC_VER) //Compiler
+//https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros
 
-    #elif defined(PTPOSIXMACH) //Mach-O Support Universal Binary, We Must Depend On The Compiler Predefined Macros To Infer The Architecture.
-        #if defined(__i386__)
-            #define PTX86
-        #elif defined(__x86_64__)
-            #define PTX64
-        #elif defined(__arm__)
-            #define PTARM
-        #elif defined(__aarch64__)
-            #define PTARM64
-        #else
-            #error 未知的架构
-        #endif
-    #else
-        #error 未知的平台
-    #endif
+//Platform
+#define PTWIN32 1
+#if defined(WINAPI_FAMILY_APP) || defined(WINAPI_FAMILY_DESKTOP_APP)
+//Has Include <winapifamily.h>
+#if (WINAPI_FAMILY==WINAPI_FAMILY_DESKTOP_APP)
+#define PTWIN32DESKTOP 1
+#elif (WINAPI_FAMILY==WINAPI_FAMILY_APP)
+#define PTWIN32RUNTIME 1
+#endif
 #else
-#error 未知的平台
+//Has Not Include <winapifamily.h>
+#if defined(WINAPI_FAMILY)
+//WINAPI_FAMILY=WINAPI_FAMILY_APP
+#define PTWIN32RUNTIME 1
+#else
+#define PTWIN32DESKTOP 1
+#endif
+#endif
+
+//Architecture
+#if defined(_M_IX86)
+#define PTX86 1
+#elif defined(_M_X64)
+#define PTX64 1
+#elif defined(_M_ARM)
+#define PTARM 1
+#elif defined(_M_ARM64)
+#define PTARM64 1
+#else
+#error Unknown Architecture //未知的架构
+#endif
+
+//CompileAndLink
+#define PTCALL __stdcall
+#define PTPTR __stdcall
+#define PTIMPORT __declspec(dllimport)
+#define PTEXPORT __declspec(dllexport)
+
+#elif defined(__GNUC__)
+//https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
+
+//Platform
+#define PTPOSIX 1
+
+#if defined(__linux__)
+#define PTPOSIXLINUX 1
+#if defined(__ANDROID__)
+#define PTPOSIXLINUXBIONIC 1
+#define PTPOSIXANDROID 1 //Obsolete
+#else
+#define PTPOSIXLINUXGLIBC 1
+#define PTPOSIXXCB 1 //Obsolete
+#endif
+#elif defined(__MACH__) 
+//https://developer.apple.com/library/archive/documentation/Porting/Conceptual/PortingUnix/compiling/compiling.html
+#define PTPOSIXMACH 1
+#else
+#error Unknown Platform //未知的平台
+#endif
+
+//Architecture //Mach-O Support Universal Binary, We Must Depend On The Compiler Predefined Macros To Infer The Architecture.
+#if defined(__i386__)
+#define PTX86 1
+#elif defined(__x86_64__)
+#define PTX64 1
+#elif defined(__arm__)
+#define PTARM 1
+#elif defined(__aarch64__)
+#define PTARM64 1
+#else
+#error Unknown Architecture //未知的架构
+#endif
+
+//CompileAndLink
+#define PTCALL
+#define PTPTR
+#define PTIMPORT
+#define PTEXPORT __attribute__((visibility("default")))
+
+#else
+#error Unknown Compiler //未知的编译器
 #endif
 
 #endif
