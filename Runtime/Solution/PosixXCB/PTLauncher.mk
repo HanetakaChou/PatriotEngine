15e383f02ec2e754db2aae31e6dbf5eb9ac6bcd8
YuqiaoZhang <yuqiaozhang@aliyun.com>
2019-11-11 02:27:01 +0800

    Linux Compile

---
+++ Runtime/Solution/PosixXCB/PTLauncher.mk
@@ -0,0 +1,1 @@
+CXXFLAGS = -x c++ -std=c++11 -pthread -fpie -pie -rtlib=libgcc -unwindlib=libgcc -stdlib=libc++ -fvisibility=hidden
---
+++ Runtime/ThirdParty/llvm/README.md
@@ -0,0 +1,1 @@
+https://github.com/YuqiaoZhang/LLVM-Prebuilt-Binaries/tree/8.0.1
