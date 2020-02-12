bfee1ef33af0108b8f33e6bdd66995dfc641bb56
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-02-12 17:05:08 +0800

    msbuild

--- Runtime/Solution/PosixLinuxGlibc/PT-MSBuild.sh
+++ Runtime/Solution/PosixLinuxGlibc/PT-MSBuild.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 2 ]; then
     MY_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
-    msbuild $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets -property:Configuration=$1 -property:Platform=$2
+    msbuild $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets/ -property:Configuration=$1 -property:Platform=$2
 else
     echo Usage: PT-MSBuild.sh Configuration\(Debug\|Release\) Platform\(x86\|x64\)
 fi
