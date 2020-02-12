e19753003e6a35806a0f093069c0cb4173813cc3
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-02-12 13:34:38 +0800

    msbuild

---
+++ Runtime/Solution/PosixLinuxGlibc/PT-MSBuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]; then
+    MY_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
+    msbuild $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets -property:Configuration=$1 -property:Platform=$2
+else
+    echo Usage: PT-MSBuild.sh Configuration\(Debug\|Release\) Platform\(x86\|x64\)
+fi
