64ce55e612fc217febe3ff73a767c3fe6614d13a
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-03-10 12:44:49 +0800

    add x86

---
+++ ThirdParty/PosixLinuxX11/build-ndk/Application-x86.mk
@@ -0,0 +1,5 @@
+# https://developer.android.com/ndk/guides/application_mk
+APP_ABI := x86
+APP_PLATFORM := android-24
+APP_STL := c++_shared
+NDK_TOOLCHAIN_VERSION := clang
---
+++ ThirdParty/PosixLinuxX11/build-ndk/build-and-run-x86.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "$(dirname "$(readlink -f "${0}")")"
+ 
+# build by ndk
+  
+rm -rf obj
+rm -rf libs
+ndk-build NDK_DEBUG=1 NDK_PROJECT_PATH:=null NDK_OUT:=obj NDK_LIBS_OUT:=libs NDK_APPLICATION_MK:=Application-x86.mk APP_BUILD_SCRIPT:=Android.mk 
+
+# before execute change the rpath to \$ORIGIN  
+  
+chrpath -r '$ORIGIN' libs/x86/a.out
+
+  
+# copy the dep libs to out dir  
+  
+cp -f ../Bionic/lib/libc.so libs/x86
+cp -f ../Bionic/lib/libdl.so libs/x86
+cp -f ../Bionic/lib/libm.so libs/x86
+cp -f ../Bionic/lib/libstdc++.so libs/x86
+
+# add the linker to cwd  
+
+cp -f ../Bionic/bin/linker .
+  
+# execute the generated a.out  
+
+libs/x86/a.out
