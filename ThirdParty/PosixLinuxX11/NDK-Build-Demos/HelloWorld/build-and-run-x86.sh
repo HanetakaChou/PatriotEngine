90524848da0b5ca7c98821e9f5895a19bd92cfd7
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-03-14 00:23:29 +0800

    fix HelloWorld ndk-build demo

--- ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-run-x86.sh
+++ ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-run-x86.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$(readlink -f "${0}")")"
  
 # build by ndk
   
-rm -rf obj/x86
+rm -rf obj/local/x86
 rm -rf libs/x86
-ndk-build APP_DEBUG:=false APP_ABI:=x86 NDK_PROJECT_PATH:=null NDK_OUT:=obj NDK_LIBS_OUT:=libs NDK_APPLICATION_MK:=Application.mk APP_BUILD_SCRIPT:=Android.mk 
+ndk-build APP_DEBUG:=false APP_ABI:=x86 NDK_PROJECT_PATH:=null NDK_OUT:=obj NDK_LIBS_OUT:=libs NDK_APPLICATION_MK:=Application.mk APP_BUILD_SCRIPT:=LinuxX11.mk 
 
 # before execute change the rpath to \$ORIGIN  
   
