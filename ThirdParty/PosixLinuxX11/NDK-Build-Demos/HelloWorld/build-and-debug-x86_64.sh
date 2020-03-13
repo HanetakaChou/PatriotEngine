98afabe9d4f44311f1bd1cbf83b4aaa4a96aa9d5
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-03-14 00:19:54 +0800

    fix HelloWorld ndk-build demo

--- ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-debug-x86_64.sh
+++ ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-debug-x86_64.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$(readlink -f "${0}")")"
  
 # build by ndk
   
-rm -rf obj
-rm -rf libs
+rm -rf obj/x86_64
+rm -rf libs/x86_64
 ndk-build APP_DEBUG:=true APP_ABI:=x86_64 NDK_PROJECT_PATH:=null NDK_OUT:=obj NDK_LIBS_OUT:=libs NDK_APPLICATION_MK:=Application.mk APP_BUILD_SCRIPT:=Android.mk 
 
 # before execute change the rpath to \$ORIGIN  
--- ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-run-x86.sh
+++ ThirdParty/PosixLinuxX11/NDK-Build-Demos/HelloWorld/build-and-run-x86.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$(readlink -f "${0}")")"
  
 # build by ndk
   
-rm -rf obj
-rm -rf libs
+rm -rf obj/x86
+rm -rf libs/x86
 ndk-build APP_DEBUG:=false APP_ABI:=x86 NDK_PROJECT_PATH:=null NDK_OUT:=obj NDK_LIBS_OUT:=libs NDK_APPLICATION_MK:=Application.mk APP_BUILD_SCRIPT:=Android.mk 
 
 # before execute change the rpath to \$ORIGIN  
