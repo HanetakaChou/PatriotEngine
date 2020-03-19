92491dc4b8a123c9ffc8ff93dba665397352e68e
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-03-19 13:03:16 +0800

    vkcube demo

--- ThirdParty/PosixLinuxX11/NDK-Build-Demos/vkCube/build-x86-release.sh
+++ ThirdParty/PosixLinuxX11/NDK-Build-Demos/vkCube/build-x86-release.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$(readlink -f "${0}")")"
 
 target_name="vkcube"
 int_dir="libs/x86"
- 
+
 # build by ndk
 rm -rf obj/local/x86
 rm -rf libs/x86
@@ -18,10 +18,26 @@ chrpath -r '$ORIGIN' ${int_dir}/${target_name}
 cp -f ../../Bionic-Redistributable/lib/libc.so ${int_dir}
 cp -f ../../Bionic-Redistributable/lib/libdl.so ${int_dir}
 cp -f ../../Bionic-Redistributable/lib/libm.so ${int_dir}
-cp -f ../../Bionic-Redistributable/lib/libstdc++.so ${int_dir}
+cp -f ../../Bionic-Redistributable/lib/libstdc++.so ${int_dir}  
 cp -f ../../Bionic-Redistributable/lib/libvulkan.so ${int_dir}  
+mkdir -p ${int_dir}/vulkan/icd.d/
+cp -f ../../Bionic-Redistributable/lib/vulkan/icd.d/intel_icd.i686.json ${int_dir}/vulkan/icd.d/  
+cp -f ../../Bionic-Redistributable/lib/libvulkan_intel.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libvulkan_radeon.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libdrm.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxcb-dri3.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxcb-randr.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libz.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libexpat.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxcb.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libX11-xcb.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxcb-present.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxcb-sync.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libxshmfence.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libc++_shared.so ${int_dir}  
+cp -f ../../Bionic-Redistributable/lib/libXau.so ${int_dir}  
 
-# place the linker at cwd  
+# place the linker at cwd   
 cp -f ../../Bionic-Redistributable/lib/linker ${int_dir}
 cd ${int_dir}
   
