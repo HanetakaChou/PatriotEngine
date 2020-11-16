e2c28b9ab9b172c5c221f0b92de364a093a7fe52
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-11-16 09:43:21 +0800

    update

---
+++ src/pt_wsi_window_posix_linux_x11.cpp
@@ -0,0 +1,8 @@
+
+// https://www.x.org/releases/X11R7.7/doc/xorg-docs/input/XKB-Config.html
+// On Linux systems, the evdev rules are most commonly used, on other systems the base rules are used.
+
+// /usr/share/X11/xkb/keycodes/evdev
+// #define KEY_ESC 9
+
+// libxkbcommon/tools/interactive-x11.c
