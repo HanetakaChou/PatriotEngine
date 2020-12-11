646a7c0fe2f9940b79d1b479373f1d218e58e44e
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-12-11 12:01:50 +0800

    accommodate the ndk-build to the glibc

---
+++ bldsys/posix_linux_x11/install_ubuntu_glibc_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+
+# 16.04.7 LTS
+# cat /etc/os-release 
+
+apt install libc6-dev
+apt install libstdc++-5-dev
+apt install gcc
+apt install g++
+apt install clang
+
+apt install libc6-dev:i386
+apt install libstdc++-5-dev:i386
