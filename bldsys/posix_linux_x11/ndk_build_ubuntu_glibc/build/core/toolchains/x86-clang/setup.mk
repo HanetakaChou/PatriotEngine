b15673328a70d4d40cec16bf7ebe56962b903f49
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-12-11 10:38:12 +0800

    accommodate the ndk-build to the glibc

---
+++ bldsys/posix_linux_x11/ndk_build_ubuntu_glibc/build/core/toolchains/x86-clang/setup.mk
@@ -0,0 +1,75 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# this file is used to prepare the NDK to build with the x86 llvm toolchain any
+# number of source files
+#
+# its purpose is to define (or re-define) templates used to build
+# various sources into target object files, libraries or executables.
+#
+# Note that this file may end up being parsed several times in future
+# revisions of the NDK.
+#
+
+#
+# Override the toolchain prefix
+#
+
+TARGET_CC := /usr/bin/clang$(HOST_EXEEXT)
+TARGET_CXX := /usr/bin/clang++$(HOST_EXEEXT)
+
+TARGET_CFLAGS := \
+    -m32 \
+    -ffunction-sections \
+    -funwind-tables \
+    -fstack-protector-strong \
+    -fPIE \
+    -fPIC \
+    -Wno-invalid-command-line-argument \
+    -Wno-unused-command-line-argument \
+    -no-canonical-prefixes \
+    -pthread \
+
+# Always enable debug info. We strip binaries when needed.
+TARGET_CFLAGS += -g
+
+# Add and LDFLAGS for the target here
+TARGET_LDFLAGS += \
+    -m32 \
+    -no-canonical-prefixes \
+    -pthread \
+
+TARGET_x86_release_CFLAGS := \
+    -O2 \
+    -DNDEBUG \
+
+# When building for debug, compile everything as x86.
+TARGET_x86_debug_CFLAGS := \
+    -O0 \
+    -UNDEBUG \
+    -fno-limit-debug-info \
+
+# This function will be called to determine the target CFLAGS used to build
+# a C or Assembler source file, based on its tags.
+#
+TARGET-process-src-files-tags = \
+$(eval __debug_sources := $(call get-src-files-with-tag,debug)) \
+$(eval __release_sources := $(call get-src-files-without-tag,debug)) \
+$(call set-src-files-target-cflags, $(__debug_sources), $(TARGET_x86_debug_CFLAGS)) \
+$(call set-src-files-target-cflags, $(__release_sources),$(TARGET_x86_release_CFLAGS)) \
+
+# The ABI-specific sub-directory that the SDK tools recognize for
+# this toolchain's generated binaries
+TARGET_ABI_SUBDIR := x86
