cbbf63c76f2f724f08ef964dd02c4c9717def36d
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-12-12 18:04:52 +0800

    update

--- bldsys/posix_linux_x11/install_ubuntu_glibc_dependencies.sh
+++ bldsys/posix_linux_x11/install_ubuntu_glibc_dependencies.sh
@@ -57,12 +57,14 @@ then
     apt-get update
 
     apt install libc6-dev:i386
+    apt install libgcc-5-dev:i386
     apt install libstdc++-5-dev:i386
     apt install libxcb1-dev:i386
     apt install libx11-dev:i386
 elif test \( \( -n "$1" \) -a \( "$1" = "x64" \) \);
 then
     apt install libc6-dev
+    apt install libgcc-5-dev
     apt install libstdc++-5-dev
     apt install libxcb1-dev
     apt install libx11-dev
