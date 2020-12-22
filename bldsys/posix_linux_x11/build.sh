0d8d465debde0223b4e5617501a83819b834f1ac
HanetakaCho <HanetakaCho@outlook.com>
2020-12-22 20:40:52 +0800

    update

--- bldsys/posix_linux_x11/build.sh
+++ bldsys/posix_linux_x11/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (C) YuqiaoZhang(HanetakaYuminaga)
+# Copyright (C) YuqiaoZhang(HanetakaCho)
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
--- src/pt_gfx_malloc_vk.cpp
+++ src/pt_gfx_malloc_vk.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) YuqiaoZhang(HanetakaYuminaga)
+ * Copyright (C) YuqiaoZhang(HanetakaCho)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
