f067411d3b66c3e12598d9421ad200009b211764
YuqiaoZhang(HanetakaYuminaga) <YuqiaoZhang@outlook.com>
2020-12-22 16:58:30 +0800

    update

--- bldsys/posix_linux_x11/install_bionic_dependencies.sh
+++ bldsys/posix_linux_x11/install_bionic_dependencies.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 #
-# Copyright (C) YuqiaoZhang
+# Copyright (C) YuqiaoZhang(HanetakaYuminaga)
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
