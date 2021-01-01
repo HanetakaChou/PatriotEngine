2a3e2df3366c45562c28ff675b601e0e955c6662
YuqiaoZhang <yuqiaozhang@aliyun.com>
2021-01-01 17:41:40 +0800

    update

--- bldsys/posix_linux_x11/Application.mk
+++ bldsys/posix_linux_x11/Application.mk
@@ -18,5 +18,7 @@
 # https://developer.android.com/ndk/guides/application_mk
 
 APP_PLATFORM := android-24
-APP_STL := c++_static #c++_shared
-NDK_TOOLCHAIN_VERSION := clang #NDK_TOOLCHAIN_VERSION := 4.9
+APP_STL := c++_static
+#APP_STL := c++_shared
+NDK_TOOLCHAIN_VERSION := clang
+#NDK_TOOLCHAIN_VERSION := 4.9
