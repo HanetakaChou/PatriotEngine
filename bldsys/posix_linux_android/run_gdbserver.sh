210203103cccf8ce76b3ba72af94ac95e66139d2
HanetakaYuminaga <HanetakaYuminaga@outlook.com>
2021-01-11 04:31:22 +0800

    update

--- bldsys/posix_linux_android/run_gdbserver.sh
+++ bldsys/posix_linux_android/run_gdbserver.sh
@@ -18,8 +18,17 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+MY_DIR="$(readlink -f "$(dirname "$0")")"
+cd ${MY_DIR}
+
 # https://developer.android.com/studio/debug#debug-types
 # android-ndk-r14b/prebuilt/windows-x86_64/bin/ndk-gdb.py
+# android-ndk-r14b/python-packages/gdbrunner/__init__.py
 # gdbrunner.get_run_as_cmd
 
-am start -a android.intent.action.MAIN -c android-intent.category.LAUNCH -n YuqiaoZhang.HanetakaYuminaga.PatriotEngine/android.app.NativeActivity
+ADB_CMD="${MY_DIR}/android-sdk/platform-tools/adb.exe"
+PACKAGE_NAME=com.Android1
+
+echo "${ADB_CMD}" shell run-as ${PACKAGE_NAME} /system/bin/sh -c pwd
+DATA_DIR="$(${ADB_CMD} shell run-as ${PACKAGE_NAME} /system/bin/sh -c pwd)"
+echo ${DATA_DIR}
