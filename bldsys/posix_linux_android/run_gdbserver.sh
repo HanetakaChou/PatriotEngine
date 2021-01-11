d9814089d3216850bee7486539e1bb615c217c5a
HanetakaYuminaga <HanetakaYuminaga@outlook.com>
2021-01-11 14:57:30 +0800

    update android debug script

--- bldsys/posix_linux_android/run_gdbserver.sh
+++ bldsys/posix_linux_android/run_gdbserver.sh
@@ -30,6 +30,8 @@ ADB_CMD="${MY_DIR}/android-sdk/platform-tools/adb"
 PACKAGE_NAME=YuqiaoZhang.HanetakaYuminaga.PatriotEngine
 LAUNCH_ACTIVITY_NAME=android.app.NativeActivity
 ARCH=arm64
+DELAY=0.25s
+PORT=5039
 
 DATA_DIR="$("${ADB_CMD}" shell "run-as "${PACKAGE_NAME}" sh -c 'pwd' 2>/dev/null" | xargs)"
 if test -z ${DATA_DIR}; then
@@ -73,6 +75,13 @@ else
     APP_GDBSERVER_PATH="${REMOTE_PATH}"
 fi
 
+# custom
+if "${ADB_CMD}" shell "run-as "${PACKAGE_NAME}" chmod 711 "${APP_GDBSERVER_PATH}""; then
+    echo "Made ${APP_GDBSERVER_PATH} executable"
+else
+    echo "Failed to make ${APP_GDBSERVER_PATH} executable"
+fi
+
 if "${ADB_CMD}" shell "ls /system/bin/readlink 1>/dev/null 2>/dev/null"; then
     if test "$("${ADB_CMD}" shell "readlink /system/bin/ps")" = "toolbox"; then
         PS_SCRIPT="ps"
@@ -106,16 +115,59 @@ fi
 
 # Launch the application if needed, and get its pid
 COMPONENT_NAME="${PACKAGE_NAME}/${LAUNCH_ACTIVITY_NAME}"
-if "${ADB_CMD}" shell "am start -D ${COMPONENT_NAME}"; then 
+if "${ADB_CMD}" shell "am start ${COMPONENT_NAME}"; then # -D # wait java
     echo "Launching activity ${COMPONENT_NAME}..."
 else
     echo "Failed to start ${COMPONENT_NAME}"
     exit 1
 fi 
 
+sleep ${DELAY} # wait the activity to launch
+
 PIDS=$("${ADB_CMD}" shell ${PS_SCRIPT} | grep "${PACKAGE_NAME}" | awk '{print $2}' | xargs) 
+LEN_PIDS=0
 for PID in ${PIDS}
 do
-    echo ${PID}
+   LEN_PIDS=$((LEN_PIDS+1))
 done
 
+if test 0 -eq ${LEN_PIDS}; then
+    echo "Failed to find running process "${PACKAGE_NAME}"" # you may increase the delay
+    exit 1
+fi
+
+if test 1 -lt ${LEN_PIDS}; then
+    echo "Multiple running processes named "${PACKAGE_NAME}""
+    exit 1
+fi
+PID=${PIDS}
+
+# Pull the linker, zygote, and notable system libraries
+
+# Start gdbserver.
+DEBUG_SOCKET="${APP_DATA_DIR}/debug_socket"
+echo "Starting gdbserver..."
+#
+# Start gdbserver in the background and forward necessary ports.
+#
+# Args:
+#   device: ADB device to start gdbserver on.
+#   gdbserver_local_path: Host path to push gdbserver from, can be None.
+#   gdbserver_remote_path: Device path to push gdbserver to.
+#   target_pid: PID of device process to attach to.
+#   run_cmd: Command to run on the device.
+#   debug_socket: Device path to place gdbserver unix domain socket.
+#   port: Host port to forward the debug_socket to.
+#   user: Device user to run gdbserver as.
+#
+#   Returns:
+#       Popen handle to the `adb shell` process gdbserver was started with.
+#
+GDBSERVER_REMOTE_PATH=${APP_GDBSERVER_PATH}
+TARGET_PID=${PID}
+
+# ${GDBSERVER_REMOTE_PATH }--once
+"${ADB_CMD}" forward "tcp:${PORT}" "localfilesystem:${DEBUG_SOCKET}"
+trap ""${ADB_CMD}" forward --remove "tcp:${PORT}"" EXIT
+
+"${ADB_CMD}" shell "run-as "${PACKAGE_NAME}" ${GDBSERVER_REMOTE_PATH} --once +"${DEBUG_SOCKET}" --attach ${TARGET_PID}"
