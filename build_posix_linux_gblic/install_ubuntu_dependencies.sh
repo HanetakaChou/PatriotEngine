0436716f2df457d1763be9648e6d89b54a06e2d7
HanetakaChou <HanetakaChou@outlook.com>
2023-03-04 05:39:56 +0100

    use lunarg vulkan sdk

--- build_posix_linux_gblic/install_ubuntu_dependencies.sh
+++ build_posix_linux_gblic/install_ubuntu_dependencies.sh
@@ -33,14 +33,16 @@ then
 fi
 
 if test \( \( -n "$1" \) -a \( "$1" = "x64" \) \);then
-    apt install libxcb1-dev
-    apt install libx11-dev
-    apt install libvulkan1
-    apt install mesa-vulkan-drivers
-    apt install vulkan-validationlayers
+    wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+    sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+    sudo apt update
+
+    sudo apt install vulkan-sdk
+    apt install glslang-tools
     apt install libvulkan-dev
 
-    apt install glslang-tools
+    apt install libxcb1-dev
+    apt install libx11-dev
 elif test \( \( -n "$1" \) -a \( "$1" = "x86" \) \);then
     dpkg --add-architecture i386
     apt-get update
